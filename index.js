2ebc84383cbf556d693491c6d0409f543ae5b642
Bekzod Sanakulov <bekzodsanakulov89@gmail.com>
2020-05-09 19:57:12 +0900

    event and eventEmitter()

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
-const fs = require('fs');
+var fs = require('fs');
+
+var event = require('events');
+const myEmitter = new event.EventEmitter();
 
 fs.readFile('text.txt', (err, data) => {                           // callback function readFie
+    myEmitter.emit('readFile');
     if (err) {
         console.log('There is an Error', err);
     }
     else {
-        setTimeout(()=> {
+        setTimeout(() => {
             console.log('Let us wait 2 seconds');
             console.log(data.toString());
-        }, 2000);
+        }, 5000);
     }
 });
 
+myEmitter.on('readFile', () => {
+    console.log('Event occured!');
+})
 console.log('We are here!');
-console.log(__filename);
 
 
 
